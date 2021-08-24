06c3d67e00df4a9a460e7dc0bcfdb32db52c5922
Joakim Eberholst <J_eberholst@hotmail.com>
2021-08-24 19:22:24 +0200

    * Header.jsx

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {ButtonNext} from "./components/ButtonNext";
-import {ButtonPrev} from "./components/ButtonPrev";
 import {ContainerButtons} from "./components/ContainerButtons";
+import {Header} from "./components/header/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header/>
       <Content/>
-
       <ContainerButtons/>
 
     </View>
---
+++ components/header/Header.jsx
@@ -0,0 +1,25 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+import {rgbaColor} from "react-native-reanimated/src/reanimated2/Colors";
+
+export const Header = () => {
+
+  return(
+     <View style={styles.content}>
+       <Text style={styles.text}>{'HEADER'}</Text>
+     </View>
+  )
+
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: rgbaColor(0,0,0, 0.3),
+  },
+  text: {
+    fontSize: 30,
+    color: '#fff',
+    textTransform: 'uppercase'
+  },
+});
+
+
